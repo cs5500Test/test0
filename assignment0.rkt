b14f8bf6487d00af9c7d94610f0a9a9820a9456b
Therapon Skoteiniotis <therapon@gmail.com>
2014-09-08 17:54:37 -0700

    Test for assignment 0
    Test for assignment 0

---
+++ assignment0.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname assignment0) (read-case-sensitive #t) (teachpacks ((lib "guess.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "hangman.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "guess.rkt" "teachpack" "htdp") (lib "master.rkt" "teachpack" "htdp") (lib "hangman.rkt" "teachpack" "htdp")))))
+ 
+;; CS2500 
+;; Assignemnt 0 
+;; Therapon Skoteiniotis (t@t.com)
